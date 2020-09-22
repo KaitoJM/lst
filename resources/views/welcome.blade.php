f171f068e05b1b0b68ee3792892738baa1e0cb4d
JM Home <johnmarkmancol@gmail.com>
2020-09-23 01:01:30 +0800

    portfolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,163 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    JOHN MARK MANCOL
-                </div>
-		<small>Web Developer</small>
-            </div>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Juan Marcos</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('style.css') }}" />
+  </head>
+  <body>
+    <div class="main-content">
+      <div class="side-bar">
+        <div class="main-information">
+          <img src="{{ asset('images/profile.png') }}" class="profile-photo" />
+          <h1>John Mark Mancol</h1>
+          <p>FULL STACK WEB DEVELOPER</p>
+          <p>
+            <i class="icon fas fa-map-marker-alt"></i> Amampacang, Calbayog
+            City, Samar
+          </p>
         </div>
-    </body>
+        <div class="description">
+          <p>
+            <i class="icon fas fa-quote-left"></i>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod nisi
+            corporis est officia neque voluptatem molestias dolore, natus vero.
+            Nulla quis ducimus quod ad delectus beatae dolorum ipsam unde est.
+          </p>
+        </div>
+        <div class="timeline">
+          <h3>TIMELINE</h3>
+          <ul>
+            <li>
+              <b>2018 FULL STACK WEB DEVELOPER</b>
+              <p>Qonvex Technology. Brgy. Obrero Calbayog City.</p>
+            </li>
+            <li>
+              <b>2015 BACKEND WEB DEVELOPER</b>
+              <p>Rootplus Technology. Brgy. Carmen Calbayog City.</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="content">
+        <ul class="navigations">
+          <li>
+            <a href="#" class="active">PROJECTS</a>
+          </li>
+          <li>
+            <a href="#">TOOLS</a>
+          </li>
+          <li>
+            <a href="#">SKILLS</a>
+          </li>
+        </ul>
+        <section class="project-container">
+          <div class="project-grid">
+            <div class="item">
+              <img src="{{ asset('images/theNinth.png') }}" alt="" />
+              <div class="project-description">
+                <h4>TheNinth</h4>
+                <p>
+                  An online comics / manga reading website also allows you to
+                  upload your own story.
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/capfrance.png') }}" alt="" />
+              <div class="project-description">
+                <h4>Capfrance</h4>
+                <p>
+                  Cap France is the leading operator of holiday villages for
+                  associative tourism in France .
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/interfaceConcept.png') }}" alt="" />
+              <div class="project-description">
+                <h4>Interface Concept</h4>
+                <p>
+                  INTERFACE CONCEPT is a leader in design and manufacturing of
+                  high-performance embedded boards and systems, aimed at ground,
+                  naval, air and industrial applications.
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/wearebaddies.png') }}" alt="" />
+              <div class="project-description">
+                <h4>WeAreBaddies</h4>
+                <p>
+                  Released in 2017, We Are Baddies is a subversive ready to wear
+                  clothing brand which cerebrates the French’s know how.
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/ceilivtv.png') }}" alt="" />
+              <div class="project-description">
+                <h4>Ceiliv TV</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/hydrofibi.png') }}" alt="" />
+              <div class="project-description">
+                <h4>HydroFibi</h4>
+                <p>
+                  Marc Thomas, former high-level athlete and holder of a STAPS
+                  pro license, uses his many years of professional experience as
+                  a teacher of aquatic...
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/ndg.png') }}" alt="" />
+              <div class="project-description">
+                <h4>NDG</h4>
+                <p>
+                  N.D.G - for nid de guêpes*, meaning " the wasp nest " -
+                  designs and produces collections meshing both american
+                  workwear culture and french high-end sensibilities.
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/guichard.png') }}" alt="" />
+              <div class="project-description">
+                <h4>Guichard</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="{{ asset('images/guichardplanning.png') }}" alt="" />
+              <div class="project-description">
+                <h4>Guichard Planning</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="tool-container hidden">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia eum
+          illo, odio at tenetur iusto expedita porro, perferendis rerum minus
+          maxime inventore facere fugiat nihil consequatur! Facilis laborum
+          autem pariatur!
+        </section>
+        <section class="skill-container hidden">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia eum
+          illo, odio at tenetur iusto expedita porro, perferendis rerum minus
+          maxime inventore facere fugiat nihil consequatur! Facilis laborum
+          autem pariatur!
+        </section>
+      </div>
+    </div>
+  </body>
 </html>
